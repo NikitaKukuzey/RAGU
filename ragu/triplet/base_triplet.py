9bf187d7ac25cace7038636dd4eb34dbced125eb
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-02-06 04:35:55 +0700

    Added base class and simple example of triplet creation

--- ragu/triplet/base_triplet.py
+++ ragu/triplet/base_triplet.py
@@ -4,7 +4,7 @@ from environment import Registrable
 
 
 class TripletExtractor(ABC, Registrable):
-    def __init__(self, config):
+    def __init__(self, config: dict=None):
         self.config = config
 
     @abstractmethod
@@ -15,14 +15,14 @@ class TripletExtractor(ABC, Registrable):
         """
         pass
 
-    def __call__(self, elements: list[str]):
+    def __call__(self, elements: list[str], client):
         """
         Обрабатывает список элементов и извлекает сущности и отношения для каждого из них.
         """
         results = []
         for index, element in enumerate(elements):
             print(f"Processing element {index + 1} of {len(elements)}")
-            result = self.extract_entities_and_relationships(element)
+            result = self.extract_entities_and_relationships(element, client)
             print("Extraction result:", result)
             results.append(result)
         return results
--- ragu/triplet/triplet_makers.py
+++ ragu/triplet/triplet_makers.py
@@ -1,18 +1,42 @@
+import opennre
+
 from ragu.triplet.base_triplet import TripletExtractor
 
 
 @TripletExtractor.register("original")
 class TripletLLM(TripletExtractor):
-    def __init__(self, config):
-        super().__init__(config)
-        self.config = config
+    def __init__(
+        self,
+        class_name: str,
+        model_name: str,
+        system_prompt: str
+    ):
+        super().__init__()
+        self.model_name = model_name
+        self.system_prompt = system_prompt
 
     def extract_entities_and_relationships(self, text, client):
-        response = self.client.chat.completions.create(
-            model=self.config.llm.model,
+        response = client.chat.completions.create(
+            model=self.model_name,
             messages=[
-                {"role": "system", "content": self.config.triplet.system_prompt},
+                {"role": "system", "content": self.system_prompt},
                 {"role": "user", "content": text}
             ]
         )
         return response.choices[0].message.content
+
+
+@TripletExtractor.register("opennre")
+class TripletNEREL(TripletExtractor):
+    def __init__(
+        self,
+        class_name: str,
+        model_name: str,
+    ):
+        super().__init__()
+        self.model_name = model_name
+        self.opennre_model = opennre.get_model(self.model_name)
+
+    def extract_entities_and_relationships(self, text):
+        result = self.opennre_model.infer({'text': text})[0]
+        return result
