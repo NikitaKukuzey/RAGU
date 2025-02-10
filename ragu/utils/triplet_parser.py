3297e84234a3747fb5b136bfec0752f015699d67
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-02-10 22:28:59 +0700

    Added parser for entity descripions

--- ragu/utils/triplet_parser.py
+++ ragu/utils/triplet_parser.py
@@ -1,8 +1,17 @@
 import re
 
+
 def parse_relations(text):
     pattern = r'\d+\.\s*(.+?)\s*<\|>\s*(.+?)\s*<\|>\s*(.+?)\s*$'
     matches = re.findall(pattern, text, re.MULTILINE)
     matches = [(entity1.lower(), relation.lower(), entity2.lower()) for entity1, relation, entity2 in matches]
     return matches
+
+def parse_description(text):
+    pattern = r"\d+\.\s*(.*)\s*<\|>\s*(.*)"
+    matches = re.findall(pattern, text, re.MULTILINE)
+    matches = [{'entity': entity.lower(), 'description': description.lower()} for entity, description in matches]
+
+    return matches
+
 
