5e0bb728b6538e565e49216c3f6a5cd18d70d7cb
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-02-12 03:20:02 +0700

    Added generation and extraction types of entities and relationship

--- ragu/utils/triplet_parser.py
+++ ragu/utils/triplet_parser.py
@@ -2,15 +2,25 @@ import re
 
 
 def parse_relations(text):
-    pattern = r'\d+\.\s*(.+?)\s*<\|>\s*(.+?)\s*<\|>\s*(.+?)\s*$'
+    pattern = r'\d+\.\s*(.+?)\s*<\|>\s*(.+?)\s*<\|>\s*(.+?)\s*<\|>\s*(.+?)\s*<\|>\s*(.+?)\s*<\|>\s*(.+?)\s*$'
     matches = re.findall(pattern, text, re.MULTILINE)
-    matches = [(entity1.lower(), relation.lower(), entity2.lower()) for entity1, relation, entity2 in matches]
+    matches = [(entity1.strip().lower(),
+                entity1_type.strip().lower(),
+                relation.strip().lower(),
+                relation_type.strip().lower(),
+                entity2.strip().lower(),
+                entity2_type.strip().lower())
+               for entity1, entity1_type, relation, relation_type, entity2, entity2_type in matches]
     return matches
+
 
 def parse_description(text):
     pattern = r"\d+\.\s*(.*)\s*<\|>\s*(.*)"
     matches = re.findall(pattern, text, re.MULTILINE)
-    matches = [{'entity': entity.lower(), 'description': description.lower()} for entity, description in matches]
+    matches = [
+        {'Entity': entity.strip().lower(),
+         'Description': description.strip().lower()}
+        for entity, description in matches]
 
     return matches
 
