651bd3656ff5146d0f0af9f0b9aeadcdab6433e7
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-02-12 20:53:21 +0700

    Added a type of community

--- ragu/common/types.py
+++ ragu/common/types.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List, Tuple, Any, Hashable
 from typing import NewType
 
 
@@ -24,4 +25,17 @@ class Relation:
     source: Node
     target: Node
     description: str
+
+
+@dataclass
+class Community:
+    """
+    Represents a community extracted from a graph.
+
+    Attributes:
+        entities: A list of tuples where each tuple contains an entity ID and its description.
+        relations: A list of tuples where each tuple represents an edge in the format (source, target, label).
+    """
+    entities: List[Tuple[Hashable, str]]
+    relations: List[Tuple[Hashable, Hashable, str]]
 
