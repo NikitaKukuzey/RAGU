361a9e2229d794cb64827389e60a3d0674898e2a
Syrenny <syrennyilya@yandex.ru>
2025-02-16 11:06:57 +0700

    Remove global imports from __init__.py

--- ragu/__init__.py
+++ ragu/__init__.py
@@ -1,9 +1,9 @@
-from ragu.chunker.base_chunker import Chunker 
-from ragu.triplet.base_triplet import TripletExtractor
-from ragu.reranker.base_reranker import Reranker
-from ragu.generate.base_generator import Generator
+from chunker.base_chunker import Chunker 
+from triplet.base_triplet import TripletExtractor
+from reranker.base_reranker import Reranker
+from generate.base_generator import Generator
 
-import ragu.chunker.chunkers
-import ragu.generate.generators
-import ragu.reranker.rerank_by_cross_encoder
-import ragu.triplet.triplet_makers
+import chunker.chunkers
+import generate.generators
+import reranker.rerank_by_cross_encoder
+import triplet.triplet_makers
