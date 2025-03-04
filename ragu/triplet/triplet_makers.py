d3ae3e7d3b8a7fc187a859038ee14f4ab3f48ebb
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-03-05 04:48:33 +0700

    Implemented batch loading in TripletLLM extractor

--- ragu/triplet/triplet_makers.py
+++ ragu/triplet/triplet_makers.py
@@ -1,5 +1,6 @@
 import logging
 
+import re
 import pandas as pd
 from typing import List, Tuple
 
@@ -9,6 +10,7 @@ from tqdm import tqdm
 import ragu.common.settings
 from ragu.common.llm import BaseLLM
 from ragu.common.decorator import no_throw
+from ragu.common.batch_generator import BatchGenerator
 from ragu.triplet.base_triplet import TripletExtractor
 
 
@@ -17,7 +19,7 @@ class TripletLLM(TripletExtractor):
     """
     A class for extracting entities and relationships (triplets) from text using a large language model (LLM).
     """
-    def __init__(self, class_name: str, validate: bool, entity_list_type: str):
+    def __init__(self, class_name: str, validate: bool, entity_list_type: str, batch_size: int):
         """
         Initializes the TripletLLM extractor.
 
@@ -25,8 +27,9 @@ class TripletLLM(TripletExtractor):
         :param validate: Whether to validate the extracted triplets (not used in this implementation).
         """
         super().__init__()
-        self.entity_list_type = entity_list_type
         self.validate = validate
+        self.batch_size = batch_size
+        self.entity_list_type = entity_list_type
 
     def extract_entities_and_relationships(self, text: List[str], client: BaseLLM) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
@@ -40,36 +43,31 @@ class TripletLLM(TripletExtractor):
         """
         from ragu.utils.default_prompts.triplet_maker_prompts import prompts
 
-        entities = []
-        relations = []
+        entities, relations = [], []
         triplet_system_prompts = prompts[self.entity_list_type]
-        for i, text in tqdm(enumerate(text), desc='Extracting entities and relationships', total=len(text)):
-            raw_data = client.generate(text, triplet_system_prompts)
+        batch_generator = BatchGenerator(text, batch_size=self.batch_size)
+        for batch in tqdm(
+                batch_generator.get_batches(),
+                desc="Index creation: extracting entities and relationships",
+                total=len(batch_generator)
+        ):
+            raw_data = client.generate(batch, triplet_system_prompts)
 
             if self.validate:
-                raw_data = self.validate_triplets(text, raw_data, client)
+                raw_data = self.validate_triplets(batch, raw_data, client)
 
             parsed_data = self._parse_llm_response(raw_data)
-
             if parsed_data is None:
                 continue
-            else:
-                current_chunk_entities, current_chunk_relations = parsed_data
 
-            # Add chunk ID to track the source of each entity and relationship
-            current_chunk_entities['chunk_id'] = i
-            current_chunk_relations['chunk_id'] = i
-
+            current_chunk_entities, current_chunk_relations = parsed_data
             entities.append(current_chunk_entities)
             relations.append(current_chunk_relations)
 
-        entities = pd.concat(entities)
-        relations = pd.concat(relations)
-
-        return entities, relations
+        return pd.concat(entities, ignore_index=True), pd.concat(relations, ignore_index=True)
 
     @no_throw
-    def validate_triplets(self, text: str, raw_triplets: str, client: BaseLLM):
+    def validate_triplets(self, text: list[str], raw_triplets: list[str], client: BaseLLM):
         """
         Validate the triplets extracted from the text using the LLM.
         :param raw_triplets:
@@ -79,55 +77,64 @@ class TripletLLM(TripletExtractor):
         """
         from ragu.utils.default_prompts.triplet_maker_prompts import validation_prompts
 
-        prompt = "Текст:\n" + text + "\n\nТриплеты:\n" + raw_triplets
+        batch_text = [f"Текст:\n{text}\n\nТриплеты:\n{raw_triplets}" for (text, raw_triplets) in zip(text, raw_triplets)]
         validation_triplet_system_prompts = validation_prompts[self.entity_list_type]
-        return client.generate(prompt, validation_triplet_system_prompts)
+        return client.generate(batch_text, validation_triplet_system_prompts)
 
 
     @no_throw
-    def _parse_llm_response(self, raw_data: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def _parse_llm_response(self, batched_raw_data: list[str]) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
         Parses the raw LLM response to extract entities and relationships.
 
-        :param raw_data: Raw response from the LLM.
+        :param batched_raw_data: Raw response from the LLM.
         :return: A tuple containing two DataFrames:
                  - The first DataFrame contains extracted entities with columns 'entity_name', 'entity_type', and 'chunk_id'.
                  - The second DataFrame contains extracted relationships with columns 'source_entity', 'target_entity', 'relationship_description', and 'chunk_id'.
         """
-        import re
 
-        sections = re.split(r'<\|\|>', raw_data)
+        @no_throw
+        def parse(raw_data: str):
+            sections = re.split(r'<\|\|>', raw_data)
+            entities_section = sections[1].strip()
+            relationships_section = sections[2].strip()
 
-        entities_section = sections[1].strip()
-        relationships_section = sections[2].strip()
+            entities, relationships = [], []
+            entity_lines = entities_section.split('\n')
+            for line in entity_lines:
+                match = re.match(r'(.+?) <\|> (.+?) <\|> (.+)', line)
+                if match:
+                    entity_name, entity_type, entity_description = match.groups()
+                    entities.append({
+                        "entity_name": entity_name.strip(),
+                        "entity_type": entity_type.strip(),
+                        "entity_description": entity_description.strip()
+                    })
 
-        entities = []
-        relationships = []
+            relationship_lines = relationships_section.split('\n')
+            for line in relationship_lines:
+                match = re.match(r'(.+?) <\|> (.+?) <\|> (.+?) <\|> (\d+)', line)
+                if match:
+                    source_entity, target_entity, relationship_description, relationship_strength = match.groups()
+                    relationships.append({
+                        "source_entity": source_entity.strip(),
+                        "target_entity": target_entity.strip(),
+                        "relationship_description": relationship_description.strip(),
+                        "relationship_strength": int(relationship_strength.strip())
+                    })
 
-        entity_lines = entities_section.split('\n')
-        for line in entity_lines:
-            match = re.match(r'(.+?) <\|> (.+?) <\|> (.+)', line)
-            if match:
-                entity_name, entity_type, entity_description = match.groups()
-                entities.append({
-                    "entity_name": entity_name.strip(),
-                    "entity_type": entity_type.strip(),
-                    "entity_description": entity_description.strip()
-                })
+            return pd.DataFrame(entities), pd.DataFrame(relationships)
 
-        relationship_lines = relationships_section.split('\n')
-        for line in relationship_lines:
-            match = re.match(r'(.+?) <\|> (.+?) <\|> (.+?) <\|> (\d+)', line)
-            if match:
-                source_entity, target_entity, relationship_description, relationship_strength = match.groups()
-                relationships.append({
-                    "source_entity": source_entity.strip(),
-                    "target_entity": target_entity.strip(),
-                    "relationship_description": relationship_description.strip(),
-                    "relationship_strength": int(relationship_strength.strip())
-                })
+        batch_entities, batch_relations  = [], []
+        for raw_data in batched_raw_data:
+            parsed_data = parse(raw_data)
+            if parsed_data is None:
+                continue
+            entities, relationships = parsed_data
+            batch_relations.append(relationships)
+            batch_entities.append(entities)
 
-        return pd.DataFrame(entities), pd.DataFrame(relationships)
+        return pd.concat(batch_entities), pd.concat(batch_relations)
 
 
 # TODO: add relation extraction and definition generation
@@ -148,7 +155,7 @@ class ComposedTripletExtractor(TripletExtractor):
         """
         extracted_entities = []
 
-        for i, text_chunk in tqdm(enumerate(texts), desc="Extracting entities and relationships"):
+        for i, text_chunk in tqdm(enumerate(texts), desc="Index creation: extracting entities and relationships"):
             entities_df = self.request_ner(text_chunk)
             entities_df["chunk_id"] = i
             extracted_entities.append(entities_df)
