c73b9a94dd4f6c788fb2cde0b7fae74cad38103e
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-03-18 00:57:54 +0700

    Prompt refactoring

--- ragu/utils/default_prompts/artifact_summarization_prompts.py
+++ ragu/utils/default_prompts/artifact_summarization_prompts.py
@@ -1,11 +1,15 @@
-entities_description_summary_prompt = """
+from collections import defaultdict
+
+artifacts_summarization_prompt = defaultdict()
+
+artifacts_summarization_prompt["summarize_entity_descriptions"] = """
 Ты - полезный ассистент. Твоя задача по данной сущности и набору фраз для её описания сгенерировать одно общее краткое описание. Оно должно быть лаконичным и непротиворечивым.
 В качестве ответа приведи только описание, не добавляя ничего лишнего.
 
 Данные:
 """
 
-relationships_description_summary_prompt = """
+artifacts_summarization_prompt["summarize_relation_descriptions"] = """
 Ты - полезный ассистент. Твоя задача по данным парам сущностей и набору фраз для описания  их отношений сгенерировать одно общее краткое описание отношений между этими сущностями.
 Оно должно быть лаконичным и непротиворечивым.
 В качестве ответа приведи только описание, не добавляя ничего лишнего.
