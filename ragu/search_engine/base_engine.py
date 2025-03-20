536cb0e9fd2dcb3546ea8da9263500f397e7e94d
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-03-20 17:40:30 +0700

    Added search engines. Now all tasks with search relevant context and getting answers is here

---
+++ ragu/search_engine/base_engine.py
@@ -0,0 +1,27 @@
+from abc import abstractmethod, ABC
+
+
+class BaseEngine(ABC):
+    def __init__(self, *args, **kwargs):
+        ...
+
+    @abstractmethod
+    def build_index(self, ):
+        """
+        Build index for knowledge graph
+        """
+        pass
+
+    @abstractmethod
+    def search(self, query, *args, **kwargs):
+        """
+        Get relevant information from knowledge graph
+        """
+        pass
+
+    @abstractmethod
+    def query(self, query: str):
+        """
+        Get answer on query from knowledge graph
+        """
+        pass
