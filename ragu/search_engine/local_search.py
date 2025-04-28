a579c31785f3774e1841251dcd0fa47ef64036ff
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-04-29 04:54:38 +0700

    Added getting relevant summaries

--- ragu/search_engine/local_search.py
+++ ragu/search_engine/local_search.py
@@ -1,7 +1,6 @@
 # Partially based on https://github.com/gusye1234/nano-graphrag/blob/main/nano_graphrag/
 
 import asyncio
-import logging
 
 from ragu.common.index import Index
 from ragu.common.llm import BaseLLM
@@ -11,9 +10,10 @@ from ragu.common.embedder import BaseEmbedder
 from ragu.search_engine.search_functional import (
     _find_most_related_text_unit_from_entities,
     _find_most_related_edges_from_entities,
+    _find_most_related_community_from_entities
 )
 
-logging.basicConfig(level=logging.INFO)
+from ragu.search_engine.types import SearchResult
 
 
 class LocalSearchEngine(BaseEngine):
@@ -44,7 +44,7 @@ class LocalSearchEngine(BaseEngine):
     async def build_index(self):
         pass
 
-    async def search(self, query: str, top_k: int = 20, *args, **kwargs) -> str:
+    async def search(self, query: str, top_k: int = 20, *args, **kwargs) -> SearchResult:
         """
         Find the most related entities/chunks/relations to the given query.
 
@@ -52,34 +52,56 @@ class LocalSearchEngine(BaseEngine):
         :param top_k: The number of relevant artifacts used to build local context.
         :return: A text report containing the most related entities/chunks/relations.
         """
-        entities = await self.index.entity_vector_db.query(query, top_k=top_k)
+        entities = await self.index.entity_vector_db.query(
+            query,
+            top_k=top_k
+        )
         nodes_data = [
             {**n, "entity_name": k["entity_name"]}
             for k in entities
             if (n := self.graph.graph.nodes.get(k["entity_name"])) is not None
         ]
-
-        chunks = await _find_most_related_text_unit_from_entities(
-            nodes_data, self.index.chunks_kv_storage, self.graph
-        )
-        relations = await _find_most_related_edges_from_entities(nodes_data, self.graph)
-
-        entity_section = "Сущность, тип сущности, описание сущности\n" + "\n".join(
-            f"{n['entity_name']}, {n.get('entity_type', 'UNKNOWN')}, {n.get('description', 'UNKNOWN')}"
-            for n in nodes_data
+        relations = await _find_most_related_edges_from_entities(
+            nodes_data,
+            self.graph
         )
 
-        relations_section = "Сущность, целевая сущность, описание отношения, ранг\n" + "\n".join(
-            f"{e['source']}, {e['target']}, {e.get('description', 'UNKNOWN')}, {e.get('rank', 'UNKNOWN')}"
-            for e in relations
+        relevant_summaries = await _find_most_related_community_from_entities(
+            nodes_data,
+            self.index.communities_kv_storage
         )
 
-        chunks_section = "\n\n".join(chunks)
-        return (
-            f"**Сущности**:\n {entity_section}\n"
-            f"**Отношения**:\n{relations_section}\n"
-            f"**Тексты**:\n{chunks_section}\n"
+        relevant_chunks = await _find_most_related_text_unit_from_entities(
+            nodes_data,
+            self.index.chunks_kv_storage,
+            self.graph
         )
+
+        relevant_entities = [
+            {
+                "entity_name": entity["entity_name"],
+                "entity_type": entity.get("entity_type", "UNKNOWN"),
+                "description": entity.get("description", "UNKNOWN")
+            } for entity in nodes_data
+        ]
+
+        relevant_relations = [
+            {
+                "source_entity": relation["source_entity"],
+                "target_entity": relation["target_entity"],
+                "description": relation.get("description", "UNKNOWN"),
+                "rank": relation.get("rank", "UNKNOWN")
+            } for relation in relations
+        ]
+
+        search_result = SearchResult(
+            entities=relevant_entities,
+            relations=relevant_relations,
+            summaries=relevant_summaries,
+            chunks=relevant_chunks
+        )
+
+        return search_result
 
     def query(self, query: str) -> str:
         """
@@ -89,8 +111,8 @@ class LocalSearchEngine(BaseEngine):
         """
         from ragu.utils.default_prompts.search_engine_query_prompts import system_prompt, query_prompts
 
-        context = asyncio.run(self.search(query))
+        context: SearchResult = asyncio.run(self.search(query))
         return self.client.generate(
-            query_prompts.format(query=query, context=context),
+            query_prompts.format(query=query, context=str(context)),
             system_prompt
         )[0]
